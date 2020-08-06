2eb9097e062b19233884960aafa798e78868eb01
Luke McCarthy <lukemccarthy@live.com>
2020-08-06 12:40:21 +0100

    Initial Commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+    <h1>Expo Tees 2019</h1>
+    <p>Welcome to the Expo Tees 2019 content management system.</p>
+    <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>&nbsp;<a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
+@endsection
+
