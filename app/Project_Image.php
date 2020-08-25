28bfcfa9ee87470de7a87055ab011e679fd3a720
Luke McCarthy <lukemccarthy@live.com>
2020-08-25 21:44:06 +0100

    Added primary key value to project image

--- app/Project_Image.php
+++ app/Project_Image.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Project_Image extends Model
 {
     protected $table = 'project_images';
-    //protected  $primaryKey = 'project_image_id';
+    protected  $primaryKey = 'project_image_id';
 
     public function project() {
         return $this->belongsTo('App\Project');
