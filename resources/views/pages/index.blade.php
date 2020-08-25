788e4951e1f2f9b104ba9771c1b2595aa716ee0c
Luke McCarthy <lukemccarthy@live.com>
2020-08-25 21:18:55 +0100

    Fixing logo on index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="jumbotron text-center">
     <h1>Expo Tees 2019</h1>
-    <a href="https://www.tees.ac.uk/expotees/" target="_blank"><img class="mx-auto d-block" style="width: 150px; height:150px;" src="/storage/cover_images/expotees.png" alt="Logo"></a>
+    <a href="https://www.tees.ac.uk/expotees/" target="_blank"><img class="mx-auto d-block" style="width: 150px; height:150px;" src="https://expotees-images.s3.eu-west-2.amazonaws.com/images/expotees.png" alt="Logo"></a>
     <p>Welcome to the Expo Tees 2019 content management system.</p>
     @guest
     <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>&nbsp;<a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
